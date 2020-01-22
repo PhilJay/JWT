340ddff6fe85670f93169155f655f1703a9f5bf1
Philipp Jahoda <phil.jahoda@gmail.com>
2020-01-22 14:06:00 +0100

    Refactoring, implement token decoding

--- src/main/java/com/philjay/apnjwt/JWT.kt
+++ src/main/java/com/philjay/apnjwt/JWT.kt
@@ -1,14 +1,14 @@
 package com.philjay.apnjwt
 
 
-import java.nio.charset.StandardCharsets
+import java.nio.charset.StandardCharsets.UTF_8
 import java.security.KeyFactory
 import java.security.Signature
 import java.security.spec.PKCS8EncodedKeySpec
+import java.util.*
 
 
 object JWT {
-
     /**
      * The encryption algorithm to be used to encrypt the token.
      */
@@ -26,7 +26,11 @@ object JWT {
      * @return A valid JWT token.
      */
     fun token(
-        teamId: String, keyId: String, secret: String, mapper: Mapper, encoder: Base64Encoder,
+        teamId: String,
+        keyId: String,
+        secret: String,
+        jsonEncoder: JsonEncoder<JWTAuthHeader, JWTAuthPayload>,
+        encoder: Base64Encoder,
         decoder: Base64Decoder
     ): String {
 
@@ -35,7 +39,7 @@ object JWT {
         val header = JWTAuthHeader(kid = keyId)
         val payload = JWTAuthPayload(teamId, now)
 
-        return token(header, payload, secret, mapper, encoder, decoder)
+        return token(header, payload, secret, jsonEncoder, encoder, decoder)
     }
 
     /**
@@ -44,26 +48,49 @@ object JWT {
      * @param header The auth header usually containing algorithm and key id.
      * @param payload The payload usually containing at least the team id and timestamp.
      * @param secret The private key (without the header and the footer - as in (BEGIN KEY...)
-     * @param mapper: A mapper to transform JWT header and payload to a json String.
+     * @param jsonEncoder: A mapper to transform JWT header and payload to a json String.
      * @param encoder An encoder to base64 encode the JWT header and payload json String.
      * @param decoder A decoder to base64 decode ByteArrays.
      * @return A valid JWT token.
      */
-    fun token(
-        header: JWTAuthHeader, payload: JWTAuthPayload, secret: String, mapper: Mapper, encoder: Base64Encoder,
+    fun <H : JWTAuthHeader, P : JWTAuthPayload> token(
+        header: H, payload: P, secret: String, jsonEncoder: JsonEncoder<H, P>, encoder: Base64Encoder,
         decoder: Base64Decoder
     ): String {
 
-        val headerString = mapper.jsonString(header)
-        val payloadString = mapper.jsonString(payload)
+        val headerString = jsonEncoder.toJson(header)
+        val payloadString = jsonEncoder.toJson(payload)
 
-        val charset = StandardCharsets.UTF_8
+        val charset = UTF_8
         val base64Header = encoder.encode(headerString.toByteArray(charset))
         val base64Payload = encoder.encode(payloadString.toByteArray(charset))
 
         val value = "$base64Header.$base64Payload"
 
         return value + "." + es256(secret, value, encoder, decoder)
+    }
+
+    /**
+     * Decodes the provided JWT token string and turns it into a JWTToken object for easy property access.
+     * @return JWT token object.
+     */
+    fun <H : JWTAuthHeader, P : JWTAuthPayload> decode(
+        jwtTokenString: String,
+        jsonDecoder: JsonDecoder<H, P>
+    ): JWTToken<H, P>? {
+        val parts = jwtTokenString.split(".")
+        return if (parts.size >= 2) {
+
+            val decoder = Base64.getDecoder()
+            val headerJson = decoder.decode(parts[0]).toString(UTF_8)
+            val payloadJson = decoder.decode(parts[1]).toString(UTF_8)
+
+            val header: H = jsonDecoder.headerFrom(headerJson)
+            val payload: P = jsonDecoder.palyoadFrom(payloadJson)
+            JWTToken(header, payload)
+        } else {
+            null
+        }
     }
 
     private fun es256(secret: String, data: String, encoder: Base64Encoder, decoder: Base64Decoder): String {
@@ -74,8 +101,7 @@ object JWT {
 
         val algECDSAsha256 = Signature.getInstance("SHA256withECDSA")
         algECDSAsha256.initSign(key)
-
-        algECDSAsha256.update(data.toByteArray(StandardCharsets.UTF_8))
+        algECDSAsha256.update(data.toByteArray(UTF_8))
 
         return encoder.encode(algECDSAsha256.sign())
     }
@@ -84,20 +110,34 @@ object JWT {
 /**
  * Mapper to transform auth header and payload to a json String.
  */
-interface Mapper {
+interface JsonEncoder<H : JWTAuthHeader, P : JWTAuthPayload> {
     /**
      * Transforms the provided header to a json String.
      * @param header The header to transform.
      * @return A json String representing the header.
      */
-    fun jsonString(header: JWTAuthHeader): String
+    fun toJson(header: H): String
 
     /**
      * Transforms the provided payload to a json String.
      * @param payload The header to transform.
      * @return A json String representing the payload.
      */
-    fun jsonString(payload: JWTAuthPayload): String
+    fun toJson(payload: P): String
+}
+
+interface JsonDecoder<H : JWTAuthHeader, P : JWTAuthPayload> {
+    /**
+     * Transforms the provided header json String into a header object.
+     * @return A header object representing the json String.
+     */
+    fun headerFrom(json: String): H
+
+    /**
+     * Transforms the provided payload json String into a payload object.
+     * @return A payload object representing the json String.
+     */
+    fun palyoadFrom(json: String): P
 }
 
 interface Base64Encoder {
@@ -117,6 +157,11 @@ interface Base64Decoder {
      */
     fun decode(bytes: ByteArray): ByteArray
 }
+
+/**
+ * JWTToken representation with header and payload. Used for String token decoding.
+ */
+open class JWTToken<H : JWTAuthHeader, P : JWTAuthPayload>(val header: H, val payload: P)
 
 /**
  * JWT Authentication token header.
---
+++ src/test/java/JWTTest.kt
@@ -0,0 +1,9 @@
+import org.junit.Test
+
+class JWTTest {
+
+    @Test
+    fun testJWT() {
+
+    }
+}
