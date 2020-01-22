ba9d0d12f89ad71a304d2cdbee3aa7810517b3ba
Philipp Jahoda <phil.jahoda@gmail.com>
2020-01-22 12:42:09 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,91 @@
+[![Release](https://img.shields.io/github/release/PhilJay/APNJWT.svg?style=flat)](https://jitpack.io/#PhilJay/APNJWT)
+
+# APNJWT
+Kotlin JWT implementation (Json Web Token) as required by APNs (Apple Push Notification Service), for use on Kotlin powered backend servers. Eases the process of creating the token based on your credentials.
+
+No other dependencies required.
+
+## Dependency 
+
+Add the following to your **build.gradle** file:
+```groovy
+allprojects {
+    repositories {
+        maven { url 'https://jitpack.io' }
+    }
+}
+
+dependencies {
+    implementation 'com.github.PhilJay:APNJWT:1.0.1'
+}
+```
+
+Or add the following to your **pom.xml**:
+
+```xml
+<repositories>
+    <repository>
+        <id>jitpack.io</id>
+        <url>https://jitpack.io</url>
+    </repository>
+</repositories>
+
+<dependency>
+    <groupId>com.github.PhilJay</groupId>
+    <artifactId>APNJWT</artifactId>
+    <version>1.0.1</version>
+</dependency>
+```
+
+## Sample Usage
+
+Create required encoders, decoders and JSON Mapper (e.g. Gson or equivalent). These are later used to properly encode or decode the token header and payload.
+
+```kotlin
+    val gson = GsonBuilder().create()
+
+    val mapper = object : Mapper {
+        override fun jsonString(header: JWTAuthHeader): String {
+            return gson.toJson(header, JWTAuthHeader::class.java)
+        }
+
+        override fun jsonString(payload: JWTAuthPayload): String {
+            return gson.toJson(payload, JWTAuthPayload::class.java)
+        }
+    }
+
+    val encoder = object : Base64Encoder {
+        override fun encode(bytes: ByteArray): String {
+            return Base64.encodeBase64String(bytes)
+        }
+    }
+
+    val decoder = object : Base64Decoder {
+        override fun decode(bytes: ByteArray): ByteArray {
+            return Base64.decodeBase64(bytes)
+        }
+    }
+```
+
+Create the token by providing your teamId, keyId and secret (private key excluding header and footer). The teamId can be obtained from the developer member center. The keyId can be obtained when you create your secret (private key).
+
+```kotlin
+    val token = JWT.token("teamId", "keyId", "secret", mapper, encoder, decoder)
+```
+
+Include the token in the authentication header when you make yor push notification request to APNs:
+
+```
+   'authentication' 'bearer $token'
+```
+
+If you are [sending pushes to iOS 13+ devices](https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/sending_notification_requests_to_apns), also include the `apns-push-type` header:
+
+```
+   'apns-push-type' 'alert' // possible values are 'alert' or 'background'
+```
+
+## Documentation
+
+For a detailed guide, please visit the [APNs documentation](https://developer.apple.com/library/archive/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/APNSOverview.html#//apple_ref/doc/uid/TP40008194-CH8-SW1) page by Apple.
+
---
+++ src/main/java/com/philjay/apnjwt/JWT.kt
@@ -0,0 +1,139 @@
+package com.philjay.apnjwt
+
+
+import java.nio.charset.StandardCharsets
+import java.security.KeyFactory
+import java.security.Signature
+import java.security.spec.PKCS8EncodedKeySpec
+
+
+object JWT {
+
+    /**
+     * The encryption algorithm to be used to encrypt the token.
+     */
+    const val algorithm = "ES256"
+
+    /**
+     * Generates a JWT token as per Apple's specifications. Does not include the required "bearer" prefix.
+     *
+     * @param teamId The team identifier (can be obtained from the developer console member center)
+     * @param keyId  The key identifier (can be obtained when generating your private key)
+     * @param secret The private key (without the header and the footer - as in (BEGIN KEY...)
+     * @param mapper: A mapper to transform JWT header and payload to a json String.
+     * @param encoder An encoder to base64 encode the JWT header and payload json String.
+     * @param decoder A decoder to base64 decode ByteArrays.
+     * @return A valid JWT token.
+     */
+    fun token(
+        teamId: String, keyId: String, secret: String, mapper: Mapper, encoder: Base64Encoder,
+        decoder: Base64Decoder
+    ): String {
+
+        val now = (System.currentTimeMillis() / 1000).toInt() // token timestamp in seconds
+
+        val header = JWTAuthHeader(kid = keyId)
+        val payload = JWTAuthPayload(teamId, now)
+
+        return token(header, payload, secret, mapper, encoder, decoder)
+    }
+
+    /**
+     * Generates a JWT token as per Apple's specifications. Does not include the required "bearer" prefix.
+     *
+     * @param header The auth header usually containing algorithm and key id.
+     * @param payload The payload usually containing at least the team id and timestamp.
+     * @param secret The private key (without the header and the footer - as in (BEGIN KEY...)
+     * @param mapper: A mapper to transform JWT header and payload to a json String.
+     * @param encoder An encoder to base64 encode the JWT header and payload json String.
+     * @param decoder A decoder to base64 decode ByteArrays.
+     * @return A valid JWT token.
+     */
+    fun token(
+        header: JWTAuthHeader, payload: JWTAuthPayload, secret: String, mapper: Mapper, encoder: Base64Encoder,
+        decoder: Base64Decoder
+    ): String {
+
+        val headerString = mapper.jsonString(header)
+        val payloadString = mapper.jsonString(payload)
+
+        val charset = StandardCharsets.UTF_8
+        val base64Header = encoder.encode(headerString.toByteArray(charset))
+        val base64Payload = encoder.encode(payloadString.toByteArray(charset))
+
+        val value = "$base64Header.$base64Payload"
+
+        return value + "." + es256(secret, value, encoder, decoder)
+    }
+
+    private fun es256(secret: String, data: String, encoder: Base64Encoder, decoder: Base64Decoder): String {
+
+        val factory = KeyFactory.getInstance("EC")
+        val keySpec = PKCS8EncodedKeySpec(decoder.decode(secret.toByteArray()))
+        val key = factory.generatePrivate(keySpec)
+
+        val algECDSAsha256 = Signature.getInstance("SHA256withECDSA")
+        algECDSAsha256.initSign(key)
+
+        algECDSAsha256.update(data.toByteArray(StandardCharsets.UTF_8))
+
+        return encoder.encode(algECDSAsha256.sign())
+    }
+}
+
+/**
+ * Mapper to transform auth header and payload to a json String.
+ */
+interface Mapper {
+    /**
+     * Transforms the provided header to a json String.
+     * @param header The header to transform.
+     * @return A json String representing the header.
+     */
+    fun jsonString(header: JWTAuthHeader): String
+
+    /**
+     * Transforms the provided payload to a json String.
+     * @param payload The header to transform.
+     * @return A json String representing the payload.
+     */
+    fun jsonString(payload: JWTAuthPayload): String
+}
+
+interface Base64Encoder {
+    /**
+     * Base64 encodes the provided bytes.
+     * @param bytes The ByteArray to be encoded.
+     * @return The encoded String.
+     */
+    fun encode(bytes: ByteArray): String
+}
+
+interface Base64Decoder {
+    /**
+     * Base64 encodes the provided bytes.
+     * @param bytes The ByteArray to be decoded.
+     * @return The decoded bytes.
+     */
+    fun decode(bytes: ByteArray): ByteArray
+}
+
+/**
+ * JWT Authentication token header.
+ */
+open class JWTAuthHeader(
+    /** the encryption algorithm used, defaults to ES256 */
+    val alg: String = JWT.algorithm,
+    /** the key identifier (found when generating private key) */
+    val kid: String
+)
+
+/**
+ * JWT authentication token payload.
+ */
+open class JWTAuthPayload(
+    /** the issuer of the token (team id found in developer member center) */
+    val iss: String,
+    /** token issued at timestamp in seconds since Epoch (UTC) */
+    val iat: Int
+)
