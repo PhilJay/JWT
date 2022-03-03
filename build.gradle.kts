d27eabd7715aa23ae26b6e188117a8c31bc45106
Philipp Jahoda <phil@ahoikapptn.com>
2022-03-03 11:33:26 +0100

    Move to build.gradle.kts & Java 14

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    java
+    kotlin("jvm") version "1.6.0"
+}
+
+group = "com.philjay.jwt"
+version = "1.2.1"
+description = "JWT"
+java.sourceCompatibility = JavaVersion.VERSION_14
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("com.google.code.gson:gson:2.9.0")
+    testImplementation("commons-codec:commons-codec:1.14")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "14"
+    }
+}
